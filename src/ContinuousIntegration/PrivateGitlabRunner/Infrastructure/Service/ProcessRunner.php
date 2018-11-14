ea5a1afa0db7fb7abe49297edbe2c4c3d9511c98
Aubry Pirian <aubry.pirian@pwc-digital.ch>
2018-11-14 21:36:54 +0100

    Log docker command (UNFINISHED)

--- src/ContinuousIntegration/PrivateGitlabRunner/Infrastructure/Service/ProcessRunner.php
+++ src/ContinuousIntegration/PrivateGitlabRunner/Infrastructure/Service/ProcessRunner.php
@@ -3,6 +3,9 @@
 namespace Madkom\ContinuousIntegration\PrivateGitlabRunner\Infrastructure\Service;
 
 use Madkom\ContinuousIntegration\PrivateGitlabRunner\Domain\Configuration\Job;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 use Symfony\Component\Process\Process;
 
 /**
@@ -10,14 +13,23 @@ use Symfony\Component\Process\Process;
  * @package Madkom\ContinuousIntegration\PrivateGitlabRunner\Infrastructure\Service
  * @author  Dariusz Gafka <d.gafka@madkom.pl>
  */
-class ProcessRunner implements \Madkom\ContinuousIntegration\PrivateGitlabRunner\Domain\Runner\ProcessRunner
+class ProcessRunner implements \Madkom\ContinuousIntegration\PrivateGitlabRunner\Domain\Runner\ProcessRunner,
+    LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
+    public function __construct()
+    {
+        $this->setLogger(new NullLogger());
+    }
 
     /**
      * @inheritdoc
      */
     public function runProcess(Job $job, $processCommand)
     {
+        var_dump($processCommand);
+
         $process = new Process($processCommand);
         $process->setTimeout(720);
 
